5ba81cf4d485f1d671a2b4d2fc5361a431f3497d
Sophia Brandt <git@s-br.net>
2023-09-02 18:11:07 +0200

    👌 IMPROVE: Update fastify autoload

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-'use strict'
+"use strict";
 
-const path = require('path')
-const AutoLoad = require('fastify-autoload')
+const path = require("path");
+const autoload = require("@fastify/autoload");
 
 module.exports = function (fastify, opts, next) {
   // Place here your custom code!
@@ -11,18 +11,18 @@ module.exports = function (fastify, opts, next) {
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
+  fastify.register(autoload, {
+    dir: path.join(__dirname, "plugins"),
     options: Object.assign({}, opts),
-  })
+  });
 
   // This loads all plugins defined in services
   // define your routes in one of these
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'services'),
+  fastify.register(autoload, {
+    dir: path.join(__dirname, "services"),
     options: Object.assign({}, opts),
-  })
+  });
 
   // Make sure to call next when done
-  next()
-}
+  next();
+};
--- src/data-access/index.js
+++ src/data-access/index.js
@@ -1,25 +1,36 @@
-const mongodb = require('mongodb')
-const configNPM = require('config')
-const { makeBookmarksDb } = require('./bookmarks-db')
+const { MongoClient } = require("mongodb");
+const configNPM = require("config");
+const { makeBookmarksDb } = require("./bookmarks-db");
 
-const dbConfig = configNPM.get('db')
+const dbConfig = configNPM.get("db");
 
-const MongoClient = mongodb.MongoClient
-const url = `mongodb://${dbConfig.host}:${dbConfig.port}`
-const database = `${dbConfig.database}`
-const client = new MongoClient(url, { useUnifiedTopology: true })
+const url = `mongodb://${dbConfig.host}:${dbConfig.port}`;
+const database = `${dbConfig.database}`;
+const client = new MongoClient(url);
+
+let cachedDb;
 
 async function makeDb() {
-  if (!client.isConnected()) {
-    await client.connect()
+  if (cachedDb) {
+    console.info("Found existing connection to database");
+    return cachedDb;
   }
-  const db = client.db(database)
-  return db
+  try {
+    console.info("Connecting to database ...");
+    await client.connect();
+    const db = client.db(database);
+    await db.command({ ping: 1 });
+    cachedDb = db;
+    return db;
+  } catch (err) {
+    console.error("Couldn't connect to database");
+    throw err;
+  }
 }
 
-const bookmarksDb = makeBookmarksDb({ makeDb, database })
+const bookmarksDb = makeBookmarksDb({ makeDb, database });
 
 module.exports = {
   bookmarksDb,
   makeDb,
-}
+};
